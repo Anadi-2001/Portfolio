06e1793ff74d9207faffb17cf2f237dae982a15b
Anadi patra <153830960+Anadi-2001@users.noreply.github.com>
2023-12-14 20:39:44 +0530

    Delete PORTFOLIO directory

--- PORTFOLIO/script.js
+++
@@ -1,9 +0,0 @@
- var typed = new Typed('#element', {
-    strings: ['Content Writer',' Java Programmer','Web Developer'],
-    typeSpeed: 50,
- });
-const spans = document.querySelectorAll('.progress span');
-spans.forEach((span) => {
-  span.style.width = span.dataset.width;
-  span.innerHTML = span.dataset.width;
-})
